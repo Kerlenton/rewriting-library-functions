883d5dfc328c01c486cf0b1dc7f4f1fed39b7836
Smekbody <usachev_semen@mail.ru>
2019-07-12 14:05:36 +0400

    initial commit

---
+++ string.h.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct wstring 
+{
+	uint8_t* data;
+	uint8_t length;
+	uint8_t how_much;
+}str_t;
+
+str_t* string_create(uint8_t length)
+{
+	str_t* string = malloc(sizeof(str_t));
+	string->data = malloc(length);             // (*string).data = ...
+	string->length = length;
+	string->how_much = 0;
+
+	return string;
+}
+
+void string_free(str_t* string)
+{
+	free(string->data);
+	free(string);
+}
+
+uint8_t string_length(str_t* string)
+{
+	return string->length;
+}
+
+void print_string(str_t* string, FILE* file)
+{
+	fwrite(string->data, sizeof(uint8_t), string->length, file);
+}
+
+str_t* string_dup(const char* str)
+{
+	str_t* string = string_create(strlen(str));
+	memcpy(string->data, str, string->length);
+	string->how_much = string->length;
+
+	return string;
+}
+
+
+str_t* concat(str_t* left, str_t* right)
+{
+	str_t* string = string_create(left->length + right->length);
+	memcpy(string->data, left->data, left->length);
+	memcpy(string->data + left->length, right->data, right->length);
+	string->how_much = string->length;
+
+	return string;
+}
