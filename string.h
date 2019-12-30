28a8e5725e36a626962ad2e2dd50e3376cf784ef
Semyon <51188729+Kerlenton@users.noreply.github.com>
2019-12-30 18:33:01 +0400

    Update string.h

--- string.h
+++ string.h
@@ -1,19 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdint.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-typedef struct wstring 
+typedef struct
 {
 	uint8_t* data;
 	uint8_t length;
 	uint8_t how_much;
 }str_t;
 
+size_t strlen(const uint8_t* string)
+{
+	size_t count = 0;
+
+	while (*string != '\0')
+	{
+		count++;
+		string++;
+	}
+}
+
 str_t* string_create(uint8_t length)
 {
-	str_t* string = (str_t*)malloc(sizeof(str_t));
-	string->data = (uint8_t*)malloc(length);             // (*string).data = ...
+	str_t* string = (str_t*) malloc(sizeof(str_t));         // (*string).data = ...
 	string->length = length;
 	string->how_much = 0;
 
@@ -31,33 +40,19 @@ uint8_t string_length(str_t* string)
 	return string->length;
 }
 
-size_t strln(const char* string)
-{
-	size_t length = 0;
-
-	while (*string != '\0')
-	{
-		length++;
-		string++;
-	}
-
-	return length;
-}
-
 void print_string(str_t* string, FILE* file)
 {
 	fwrite(string->data, sizeof(uint8_t), string->length, file);
 }
 
-str_t* string_dup(const char* str)
+str_t* strdup(const uint8_t* str)
 {
-	str_t* string = string_create((uint8_t)strlen(str));
+	str_t* string = string_create((uint8_t) strlen(str));
 	memcpy(string->data, str, string->length);
 	string->how_much = string->length;
 
 	return string;
 }
-
 
 str_t* concat(str_t* left, str_t* right)
 {
@@ -71,7 +66,7 @@ str_t* concat(str_t* left, str_t* right)
 
 str_t* append(str_t* left, str_t* right)
 {
-	str_t* space = string_dup(" ");
+	str_t* space = strdup(" ");
 	str_t* string = string_create(left->length + right->length + space->length);
 	memcpy(string->data, left->data, left->length);
 	memcpy(string->data + left->length, space->data, space->length);
@@ -79,4 +74,4 @@ str_t* append(str_t* left, str_t* right)
 	string->how_much = string->length;
 
 	return string;
-}
+}}
