ff92e0084fb2a4ec0158b99183a5843470481964
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2020-10-30 08:33:06 +0400

    Update cstring.c

--- cstring/string/cstring.c
+++ cstring/string/cstring.c
@@ -3,6 +3,8 @@
 static void template_print(char *string, size_t length, FILE *file);
 static void template_concat(char *string, char *left, char *right, size_t length_l, size_t length_r);
 static void template_append(char *string, char *left, char *right, char *concat_space, size_t length_l, size_t length_r);
+static void template_reverse(char *string, size_t length);
+static size_t template_compare(char *string1, char *string2, size_t length1, size_t length2);
 
 extern str_t *string_create(size_t length)
 {
@@ -29,11 +31,6 @@ extern size_t chars_length(char *chars)
     return strlen(chars);
 }
 
-static void template_print(char *string, size_t length, FILE *file)
-{
-    fwrite(string, sizeof(char), length, file);
-}
-
 extern void string_print(str_t *string, FILE *file)
 {
     template_print(string->data, string->length, file);
@@ -42,6 +39,11 @@ extern void string_print(str_t *string, FILE *file)
 extern void chars_print(char *string, FILE *file)
 {
     template_print(string, chars_length(string), file);
+}
+
+static void template_print(char *string, size_t length, FILE *file)
+{
+    fwrite(string, sizeof(char), length, file);
 }
 
 extern str_t *string_dup(const char *str)
@@ -63,12 +65,6 @@ extern char *string_pud(str_t *string)
     return str;
 }
 
-static void template_concat(char *string, char *left, char *right, size_t length_l, size_t length_r)
-{
-    memcpy(string, left, length_l);
-    memcpy(string + length_l, right, length_r);
-}
-
 extern str_t *string_concat(str_t *left, str_t *right)
 {
     str_t *string = string_create(left->length + right->length);
@@ -86,12 +82,11 @@ extern char *chars_concat(char *left, char *right)
     return chars;
 }
 
-static void template_append(char *string, char *left, char *right, char *concat_space, size_t length_l, size_t length_r)
+static void template_concat(char *string, char *left, char *right, size_t length_l, size_t length_r)
 {
     memcpy(string, left, length_l);
-    memcpy(string + length_l, concat_space, length_r);
+    memcpy(string + length_l, right, length_r);
 }
-
 
 extern str_t *string_append(str_t *left, str_t *right)
 {
@@ -111,35 +106,54 @@ extern char *chars_append(char *left, char *right)
     return chars;
 }
 
-extern void reverse(str_t *string)
+static void template_append(char *string, char *left, char *right, char *concat_space, size_t length_l, size_t length_r)
+{
+    memcpy(string, left, length_l);
+    memcpy(string + length_l, concat_space, length_r);
+}
+
+extern void string_reverse(str_t *string)
+{
+    template_reverse(string->data, string->length);
+}
+
+extern void chars_reverse(char *chars)
+{
+    template_reverse(chars, chars_length(chars));
+}
+
+static void template_reverse(char *string, size_t length)
 {
     uint8_t c;
     size_t i, j;
 
-    for (i = 0, j = string->length - 1; i < j; i++, j--)
+    for (i = 0, j = length - 1; i < j; i++, j--)
     {
-        c = *(string->data + i);
-        *(string->data + i) = *(string->data + j);
-        *(string->data + j) = c;
+        c = *(string + i);
+        *(string + i) = *(string + j);
+        *(string + j) = c;
     }
 }
 
-extern int string_compare(str_t *string1, str_t *string2)
+extern size_t string_compare(str_t *string1, str_t *string2)
 {
-    size_t max_len = string1->length > string2->length ? (string2->length) : (string1->length);
+    template_compare(string1->data, string2->data, string1->length, string2->length);
+}
 
-    for (int i = 0; (*(string1->data) == *(string2->data)) && (i < max_len); i++, (string1->data)++, (string2->data)++)
+extern size_t chars_compare(char *chars1, char *chars2)
+{
+    template_compare(chars1, chars2, chars_length(chars1), chars_length(chars2));
+}
+
+static size_t template_compare(char *string1, char *string2, size_t length1, size_t length2)
+{
+    size_t max_len = length1 > length2 ? length2 : length1;
+
+    for (size_t i = 0; *string1 == *string2 && (i < max_len); i++, string1++, string2++)
     {
         if ((max_len - 1) == i)
             return 0;
     }
 
-    return *(string1->data) - *(string2->data);
-}
-
-extern void string_copy(str_t *string1, str_t *string2)
-{
-    string_free(string1);
-    string1 = string_create(string2->length);
-    memcpy(string1->data, string2->data, string2->length);
+    return *string1 - *string2;
 }
