c99e4da77f2b1a401fb6a609aa041396a34a4745
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2020-10-30 14:42:23 +0400

    Update cstring.h

--- cstring/string/cstring.h
+++ cstring/string/cstring.h
@@ -16,14 +16,18 @@ typedef struct String
 extern str_t *string_create(size_t length);
 extern void string_free(str_t *string);
 
+extern char *chars_create(size_t length);
+extern void *chars_free(char *chars);
+
 extern size_t string_length(str_t *string);
-extern size_t chars_length(char *chars);
+extern size_t chars_length(const char *chars);
 
 extern void string_print(str_t *string, FILE *file);
 extern void chars_print(char *string, FILE *file);
 
 extern str_t *string_dup(const char *str);
 extern char *string_pud(str_t *string);
+extern char *chars_dup(const char *chars);
 
 extern str_t *string_concat(str_t *left, str_t *right);
 extern char *chars_concat(char *left, char *right);
