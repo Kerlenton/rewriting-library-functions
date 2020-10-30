e4070ff5c57b874cc32aa879752b31e8785ca488
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2020-10-30 14:41:37 +0400

    Update main.c

--- cstring/main.c
+++ cstring/main.c
@@ -2,21 +2,21 @@
 
 int main(void)
 {
-    //str_t *hello = string_dup("hello");
-    //str_t *hello1 = string_dup("ape");
+    str_t *hello = string_dup("hello");
+    str_t *hello1 = string_dup("ape");
 
-   // string_copy(hello, hello1);
+    str_t *app = string_concat(hello, hello1);
+    string_print(app, stdout);
 
-    //printf("%d", sizeof(uint8_t));
-   // str_t *app = string_concat(hello, hello);
-   // string_print(app, stdout);
+    putchar('\n');
+    string_reverse(hello);
+    string_print(hello, stdout);
+    putchar('\n');
+    printf("%s", chars_append("hello", "world"));
+    putchar('\n');
 
-   //string_reverse(hello);
-   //string_print(hello, stdout);
-   //printf("%s", chars_append("hello", "world"));
-
-   char *s = "hello";
-   char *s1 = "bye";
+   char *s = chars_dup("hello world");
+   printf("%s", s);
 
    return 0;
 }
