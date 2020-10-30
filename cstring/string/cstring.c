d6a26999149d5e3a7e1359675e0444777ce398d4
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2020-10-30 14:42:03 +0400

    Update cstring.c

--- cstring/string/cstring.c
+++ cstring/string/cstring.c
@@ -9,7 +9,7 @@ static size_t template_compare(char *string1, char *string2, size_t length1, siz
 extern str_t *string_create(size_t length)
 {
     str_t *string = (str_t*)malloc(sizeof(str_t));
-    string->data = (char*)malloc(length);
+    string->data = (char*)malloc(length + 1);
     string->length = length;
     string->how_much = 0;
 
@@ -21,12 +21,25 @@ extern void string_free(str_t *string)
     free(string->data);
     free(string);
 }
+
+extern char *chars_create(size_t length)
+{
+    char *chars = (char*)malloc(length + 1);
+
+    return chars;
+}
+
+extern void *chars_free(char *chars)
+{
+    free(chars);
+}
+
 extern size_t string_length(str_t *string)
 {
     return string->length;
 }
 
-extern size_t chars_length(char *chars)
+extern size_t chars_length(const char *chars)
 {
     return strlen(chars);
 }
@@ -48,19 +61,25 @@ static void template_print(char *string, size_t length, FILE *file)
 
 extern str_t *string_dup(const char *str)
 {
-    str_t *string = string_create(strlen(str));
-    memcpy(string->data, str, string->length);
+    str_t *string = string_create(chars_length(str));
+    memcpy(string->data, str, string->length + 1);
     string->how_much = string->length;
 
     return string;
 }
 
+extern char *chars_dup(const char *chars)
+{
+    char *str = chars_create(chars_length(chars));
+    memcpy(str, chars, chars_length(chars) + 1);
+
+    return str;
+}
+
 extern char *string_pud(str_t *string)
 {
-    char *str = (char*)malloc(string->length);
-    memcpy(str, string->data, string->length);
-
-    *(str + string->length) = '\0';
+    char *str = chars_create(string->length);
+    memcpy(str, string->data, string->length + 1);
 
     return str;
 }
@@ -76,8 +95,8 @@ extern str_t *string_concat(str_t *left, str_t *right)
 
 extern char *chars_concat(char *left, char *right)
 {
-    char *chars = (char*)malloc(chars_length(left) + chars_length(right));
-    template_concat(chars, left, right, chars_length(left), chars_length(right) + 1);
+    char *chars = chars_create(chars_length(left) + chars_length(right));
+    template_concat(chars, left, right, chars_length(left), chars_length(right));
 
     return chars;
 }
@@ -85,14 +104,16 @@ extern char *chars_concat(char *left, char *right)
 static void template_concat(char *string, char *left, char *right, size_t length_l, size_t length_r)
 {
     memcpy(string, left, length_l);
-    memcpy(string + length_l, right, length_r);
+    memcpy(string + length_l, right, length_r + 1);
 }
 
 extern str_t *string_append(str_t *left, str_t *right)
 {
     str_t *space = string_dup(" ");
     str_t *string = string_create(left->length + right->length + 1);
-    template_append(string->data, left->data, right->data, string_concat(space, right)->data, left->length, right->length + 2);
+
+    template_append(string->data, left->data, right->data, string_concat(space, right)->data, left->length, right->length);
+
     string->how_much = string->length;
     string_free(space);
 
@@ -101,15 +122,16 @@ extern str_t *string_append(str_t *left, str_t *right)
 
 extern char *chars_append(char *left, char *right)
 {
-    char *chars = (char*)malloc(chars_length(left) + chars_length(right) + 1);
-    template_append(chars, left, right, chars_concat(" ", right), chars_length(left), chars_length(right) + 2);
+    char *chars = chars_create(chars_length(left) + chars_length(right) + 1);
+    template_append(chars, left, right, chars_concat(" ", right), chars_length(left), chars_length(right));
+
     return chars;
 }
 
 static void template_append(char *string, char *left, char *right, char *concat_space, size_t length_l, size_t length_r)
 {
     memcpy(string, left, length_l);
-    memcpy(string + length_l, concat_space, length_r);
+    memcpy(string + length_l, concat_space, length_r + 2);
 }
 
 extern void string_reverse(str_t *string)
